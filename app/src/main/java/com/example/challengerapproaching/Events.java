38f73946a8c62c0cda5201d258ec6b03b347b96b
perpetuallink <samack1011@aim.com>
2020-03-07 03:59:29 -0500

    Created a scroll view that gets dynamically filled with EditText views once a date is selected.

--- app/src/main/java/com/example/challengerapproaching/Events.java
+++ app/src/main/java/com/example/challengerapproaching/Events.java
@@ -7,8 +7,9 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
@@ -19,10 +20,15 @@ public class Events extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private TextView eventSetup;
     private DatePickerDialog.OnDateSetListener onDateSetListener;
+    private LinearLayout lView;
+    private int numDates = 0;
+    private String dateName;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.events);
         eventSetup = findViewById(R.id.tvEvent);
+        lView = findViewById(R.id.dateList);
         eventSetup.setOnClickListener(v -> {
             Calendar cal = Calendar.getInstance();
             int year = cal.get(Calendar.YEAR);
@@ -35,14 +41,27 @@ public class Events extends AppCompatActivity {
             dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
             dialog.show();
         });
+
         onDateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 month = month + 1;
+                numDates++;
+                dateName = "Date" + numDates + ": ";
+                EditText newDate = new EditText(Events.this);
+                newDate.setId(numDates);
+
                 Log.d(TAG, "onDateSet: mm/dd/yyyy: " + month + "/" + dayOfMonth + "/" + year);
                 String date = month + "/" + dayOfMonth + "/" + year;
-                eventSetup.setText(date);
+                //eventSetup.setText((date));
+                newDate.setTextSize(30);
+                newDate.setText(dateName + date);
+
+
+                lView.addView(newDate);
             }
         };
-    }
+
+    };
+
 }
