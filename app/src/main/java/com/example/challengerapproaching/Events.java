1ef7db6bb6b309d05ad00b18f49b3a08cb7e4c6f
perpetuallink <samack1011@aim.com>
2020-03-07 02:27:31 -0500

    Event button allows user to select a date. Displays the date selected. Basic, but some functionality.

--- app/src/main/java/com/example/challengerapproaching/Events.java
+++ app/src/main/java/com/example/challengerapproaching/Events.java
@@ -2,30 +2,47 @@ package com.example.challengerapproaching;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
-import static android.view.View.*;
+
+import java.util.Calendar;
 
 public class Events extends AppCompatActivity {
-    Button setdate;
 
+    private static final String TAG = "MainActivity";
+    private TextView eventSetup;
+    private DatePickerDialog.OnDateSetListener onDateSetListener;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.events);
+        eventSetup = findViewById(R.id.tvEvent);
+        eventSetup.setOnClickListener(v -> {
+            Calendar cal = Calendar.getInstance();
+            int year = cal.get(Calendar.YEAR);
+            int month = cal.get(Calendar.MONTH);
+            int day = cal.get(Calendar.DAY_OF_MONTH);
 
-
-        setdate = (Button) findViewById(R.id.daychoice);
-        setdate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(v.getId() == R.id.daychoice) {
-                    v.setId(R.id.calendarView2);
-                    v.setVisibility(VISIBLE);
-                }
-            }
+            DatePickerDialog dialog = new DatePickerDialog(
+                    Events.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                    onDateSetListener, year, month, day);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.show();
         });
+        onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: mm/dd/yyyy: " + month + "/" + dayOfMonth + "/" + year);
+                String date = month + "/" + dayOfMonth + "/" + year;
+                eventSetup.setText(date);
+            }
+        };
     }
 }
