769aa738f3fb874c436ba71aef8f1d3b1cda842a
tristankingsley <46898839+tristankingsley@users.noreply.github.com>
2020-03-07 20:29:22 -0500

    Checkstyle for events.java complete

--- app/src/main/java/com/example/challengerapproaching/Events.java
+++ app/src/main/java/com/example/challengerapproaching/Events.java
@@ -1,7 +1,5 @@
 package com.example.challengerapproaching;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.DatePickerDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -11,57 +9,56 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
-
+import androidx.appcompat.app.AppCompatActivity;
 import java.util.Calendar;
 
 public class Events extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
-    private TextView eventSetup;
-    private DatePickerDialog.OnDateSetListener onDateSetListener;
-    private LinearLayout lView;
-    private int numDates = 0;
-    private String dateName;
+  private static final String TAG = "MainActivity";
+  private TextView eventSetup;
+  private DatePickerDialog.OnDateSetListener onDateSetListener;
+  private LinearLayout alView;
+  private int numDates = 0;
+  private String dateName;
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.events);
-        eventSetup = findViewById(R.id.tvEvent);
-        lView = findViewById(R.id.dateList);
-        eventSetup.setOnClickListener(v -> {
-            Calendar cal = Calendar.getInstance();
-            int year = cal.get(Calendar.YEAR);
-            int month = cal.get(Calendar.MONTH);
-            int day = cal.get(Calendar.DAY_OF_MONTH);
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.events);
+    eventSetup = findViewById(R.id.tvEvent);
+    alView = findViewById(R.id.dateList);
+    eventSetup.setOnClickListener(v -> {
+      Calendar cal = Calendar.getInstance();
+      int year = cal.get(Calendar.YEAR);
+      int month = cal.get(Calendar.MONTH);
+      int day = cal.get(Calendar.DAY_OF_MONTH);
 
-            DatePickerDialog dialog = new DatePickerDialog(
+      DatePickerDialog dialog = new DatePickerDialog(
                     Events.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,
                     onDateSetListener, year, month, day);
-            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-            dialog.show();
-        });
+      dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+      dialog.show();
+    });
 
-        onDateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                month = month + 1;
-                numDates++;
-                dateName = "Date" + numDates + ": ";
-                EditText newDate = new EditText(Events.this);
-                newDate.setId(numDates);
+    onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+      @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+          month = month + 1;
+          numDates++;
+          dateName = "Date" + numDates + ": ";
+          EditText newDate = new EditText(Events.this);
+          newDate.setId(numDates);
 
-                Log.d(TAG, "onDateSet: mm/dd/yyyy: " + month + "/" + dayOfMonth + "/" + year);
-                String date = month + "/" + dayOfMonth + "/" + year;
-                //eventSetup.setText((date));
-                newDate.setTextSize(30);
-                newDate.setText(dateName + date);
+          Log.d(TAG, "onDateSet: mm/dd/yyyy: " + month + "/" + dayOfMonth + "/" + year);
+          String date = month + "/" + dayOfMonth + "/" + year;
+          newDate.setTextSize(30);
+          newDate.setText(dateName + date);
 
 
-                lView.addView(newDate);
-            }
+          alView.addView(newDate);
+        }
         };
 
-    };
+  }
 
+  ;
 }
