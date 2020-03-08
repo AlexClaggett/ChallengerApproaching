3f91f6fa0d432f53eef39a5e5a1b3520e5d20bfb
tristankingsley <46898839+tristankingsley@users.noreply.github.com>
2020-03-08 15:12:52 -0400

    Javadoc descriptions for both packages

---
+++ app/src/main/java/com/example/challengerapproaching/characters/package-info.java
@@ -0,0 +1,2 @@
+/** This package contains all character classes that are called from the character select screeen.*/
+package com.example.challengerapproaching.characters;
---
+++ app/src/main/java/com/example/challengerapproaching/package-info.java
@@ -0,0 +1,2 @@
+/** This package contains a class to set up the character select screen and the events page.*/
+package com.example.challengerapproaching;
