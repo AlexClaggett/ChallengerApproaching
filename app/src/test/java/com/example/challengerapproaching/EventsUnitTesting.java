7bd34f38afb851366647b59470927ebe37af4c81
PerpetualLink <Samack1011@gmail.com>
2020-04-17 06:59:03 -0400

    JavaDoc for all event classes and checkstyle done. Checkstyle has issues with initial javadoc indentation that i'm a little confused with, but everything else for checkstyle's is okay with.

---
+++ app/src/test/java/com/example/challengerapproaching/EventsUnitTesting.java
@@ -0,0 +1,49 @@
+package com.example.challengerapproaching;
+
+import com.example.challengerapproaching.utils.Event;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class EventsUnitTesting {
+    @Test
+    public void Event_Default_Constructor (){
+        Event e = new Event();
+        Assert.assertEquals("None", e.getName());
+        Assert.assertEquals("None",e.getDate());
+        Assert.assertEquals(-1, e.getId());
+    }
+    @Test
+    public void Event_NO_ID_Constructor (){
+        String name = "Smash";
+        String date = "4/17/2020";
+        Event e = new Event(name, date);
+        Assert.assertEquals(name, e.getName());
+        Assert.assertEquals(date,e.getDate());
+        Assert.assertEquals(-1, e.getId());
+    }
+    @Test
+    public void Event_WITH_ID_Constructor (){
+        int id = 5;
+        String name = "Smush";
+        String date = "4/20/2020";
+        Event e = new Event(id, name, date);
+        Assert.assertEquals(name, e.getName());
+        Assert.assertEquals(date,e.getDate());
+        Assert.assertEquals(id, e.getId());
+    }
+    @Test
+    public void Event_Setters (){
+        Event e = new Event(5,"Smash", "4/15/2020");
+        e.setName("S");
+        Assert.assertEquals("S", e.getName());
+        e.setDate("4/31/2020");
+        Assert.assertEquals("4/31/2020", e.getDate());
+        e.setId(123);
+        Assert.assertEquals(123, e.getId());
+    }
+}
