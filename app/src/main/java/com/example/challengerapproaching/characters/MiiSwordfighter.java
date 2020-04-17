e2117fb4baabb63d3a63677cb0a12220af63cb05
tristankingsley <46898839+tristankingsley@users.noreply.github.com>
2020-04-17 00:53:05 -0400

    King K Rool Up moves added Mii swordfighter bug fixed

--- app/src/main/java/com/example/challengerapproaching/characters/MiiSwordfighter.java
+++ app/src/main/java/com/example/challengerapproaching/characters/MiiSwordfighter.java
@@ -1,14 +1,18 @@
 package com.example.challengerapproaching.characters;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.challengerapproaching.R;
 import com.example.challengerapproaching.utils.GifImageView;
 import java.io.IOException;
+import java.io.InputStream;
 
 /** This class sets the content view to Mii Swordfighter's xml
  * file when the respective button is pushed.*/
@@ -76,10 +80,25 @@ public class MiiSwordfighter extends AppCompatActivity {
       public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         GifImageView gifImageView = findViewById(R.id.GifImageView);
         String toUrl = "https://ultimateframedata.com/hitboxes/mii_swordfighte/" + miiSwordfighterMoves[position];
-        try {
-          gifImageView.setGifImageUrl(toUrl);
-        } catch (IOException e) {
-          e.printStackTrace();
+        ImageView imageView = findViewById(R.id.imageView3);
+        if (toUrl.endsWith("png")) {
+          gifImageView.setVisibility(View.INVISIBLE);
+          try {
+            InputStream inputStream = new java.net.URL(toUrl).openStream();
+            Bitmap bm = BitmapFactory.decodeStream(inputStream);
+            imageView.setImageBitmap(bm);
+            imageView.setVisibility(View.VISIBLE);
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+        } else {
+          imageView.setVisibility(View.INVISIBLE);
+          try {
+            gifImageView.setGifImageUrl(toUrl);
+            gifImageView.setVisibility(View.VISIBLE);
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
         }
       }
 
